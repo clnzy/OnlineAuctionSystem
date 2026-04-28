cfac4a24da7ea25e24ec524b6bf9a7d0565936bf
Clinzy <25023420@vnu.edu.vn>
2026-04-28 21:25:52 +0700

    Update AuctionStatus.java

--- OnlineAuctionSystem/src/main/java/models/AuctionStatus.java
+++ OnlineAuctionSystem/src/main/java/models/AuctionStatus.java
@@ -1,10 +1,9 @@
 package models;
 
-// Enum định nghĩa các trạng thái của phiên đấu giá (Theo yêu cầu mục 3.1.4)
 public enum AuctionStatus {
-    OPEN,      // Mới mở
-    RUNNING,   // Đang diễn ra
-    FINISHED,  // Đã kết thúc
-    PAID,      // Đã thanh toán
-    CANCELED   // Đã hủy
+    OPEN,
+    RUNNING,
+    FINISHED,
+    PAID,
+    CANCELED
 }
